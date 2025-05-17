42d468f112d23826aad6be1d2b1b1a83ec9713d8
Lorel12 <148823159+Lorel12@users.noreply.github.com>
2025-05-17 14:38:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+"""
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import hashlib
 import re
@@ -14,6 +15,10 @@ import os, json, sqlite3
 from werkzeug.utils import secure_filename
 from datetime import datetime
 from dateutil import parser
+"""
+from utils import extraction_du_texte, division_en_phrases, recherche_mots_cles
+from scraper import extract_paragraphs_from_url, extract_links_from_url, analyse_site
+from graph import generate_graph
 
 user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 headers = {"User-Agent": user_agent}
