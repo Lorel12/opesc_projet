d2f5a34c172b593f19bbd7144922bfdd464b348a
Lorel12 <148823159+Lorel12@users.noreply.github.com>
2025-05-17 14:31:18 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,1 +1,21 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+import hashlib
+import re
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import pandas as pd
+import matplotlib
+matplotlib.use('Agg')  
+import matplotlib.pyplot as plt
+import PyPDF2, docx
+from collections import defaultdict
+import os, json, sqlite3
+from werkzeug.utils import secure_filename
+from datetime import datetime
+from dateutil import parser
+
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+headers = {"User-Agent": user_agent}
+
 
